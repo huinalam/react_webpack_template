22e767fe3d4a312e3d8175659d342dab7640b856
huinalam <huinalam@gmail.com>
2018-01-03 11:54:05 +0900

    stylus 적용

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,11 @@
 // Dependencies
 import React from 'react';
+import './Header.styl';
 
 class Header extends React.Component {
     render() {
-        return <div>
+        return <div className="header">
             <h1>Hello, world!!!!</h1>
-            <p>Wellcome to Dotnet Core World!</p>
             <p>Wellcome to Dotnet Core World!</p>
             <p>Wellcome to Dotnet Core World!</p>
             <p>Wellcome to Dotnet Core World!</p>
--- webpack.config.js
+++ webpack.config.js
@@ -3,53 +3,66 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
-module.exports = {
-    entry: {
-        app: ['babel-polyfill', 'react-hot-loader/patch', './src/index.jsx']
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-        hot: true
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            title: 'Development'
-        }),
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                include: /src/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            babelrc: true,
-                            presets: [
-                                ['env', { modules: false }],
-                                'react',
-                            ]
+module.exports = (env) => {
+    const isDevBuild = !(env && env.prod);
+    
+    return [{
+        entry: {
+            app: ['babel-polyfill', 'react-hot-loader/patch', './src/index.jsx']
+        },
+        devtool: 'inline-source-map',
+        devServer: {
+            contentBase: './dist',
+            hot: true
+        },
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        plugins: [
+            new CleanWebpackPlugin(['dist']),
+            new HtmlWebpackPlugin({
+                title: 'Development'
+            }),
+            new webpack.NamedModulesPlugin(),
+            new webpack.HotModuleReplacementPlugin()
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    include: /src/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                            options: {
+                                babelrc: true,
+                                presets: [
+                                    ['env', { modules: false }],
+                                    'react',
+                                ]
+                            }
                         }
-                    }
-                ],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            }
-        ]
-    },
-};
+                    ],
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.styl$/,
+                    use: isDevBuild
+                        ? ['style-loader', 'css-loader', 'stylus-loader']
+                        : ExtractTextPlugin.extract({
+                            fallback: 'style-loader',
+                            use: ['css-loader', 'stylus-loader']
+                        })
+                },
+            ]
+        }
+    }];
+}
